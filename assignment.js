913892e940aa04ed9b7d06a9862e7960528373eb
ArzuAshik <arzu2050@gmail.com>
2020-07-16 23:36:36 +0600

    brickCalculator done

--- assignment.js
+++ assignment.js
@@ -17,7 +17,21 @@ function woodCalculator(chair, table, bed){
 }
 
 function brickCalculator(floor){
-
+    let brick = 0;
+    if(floor > 0 && floor <= 10){
+        brick = floor * 15000;
+    }
+    else if(floor > 10 && floor <= 20){
+        floor = floor - 10;
+        brick = (floor * 12000) + 150000;
+    }
+    else if(floor > 20){
+        floor = floor - 20;
+        brick = (floor * 10000) + 270000;
+    }else{
+        return "Could not be a negative value."
+    }
+    return brick;
 }
 
 function tinyFriend(friends){
@@ -27,3 +41,5 @@ function tinyFriend(friends){
 console.log(feetToMile(65));
 
 console.log(woodCalculator(1, 3, 5));
+
+console.log(brickCalculator(25));
