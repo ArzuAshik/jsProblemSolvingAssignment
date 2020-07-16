97b750b4044608f581737946bdb338b1a8fe94de
ArzuAshik <arzu2050@gmail.com>
2020-07-16 23:07:34 +0600

    feetToMile done

--- assignment.js
+++ assignment.js
@@ -1,5 +1,10 @@
 function feetToMile(feet){
-
+    if(feet >= 0){
+        const mile = feet / 5280;
+        return mile;
+    }else{
+        return "Please Enter a valid Value of Foot";
+    }
 }
 
 function woodCalculator(chair, table, bed){
@@ -11,5 +16,7 @@ function brickCalculator(floor){
 }
 
 function tinyFriend(friends){
-    
+
 }
+
+console.log(feetToMile(65));
